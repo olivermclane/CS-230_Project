54e5ca8ccabdb6fffabb9c39dc0b805d854b214b
dustgard <77804262+dustgard@users.noreply.github.com>
2021-10-04 15:59:11 -0600

    Start of upload

---
+++ CS-230_Game/src/flightGame/PlaneSprite.java
@@ -0,0 +1,233 @@
+/**
+ * 
+ */
+package flightGame;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+import java.io.File;
+import java.io.IOException;
+
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+
+public class PlaneSprite extends SpriteSheet implements ActionListener{
+	private BufferedImage [] bankLeft;
+	private BufferedImage [] leftLevelout;
+	private BufferedImage [] bankRight;
+	private BufferedImage [] rightLevelout;
+	
+	private Rectangle bounds;
+	private int hitsTaken;
+	private int rightMovement;
+	private int leftMovement;
+	private boolean planeHit;
+	private boolean planeDestroyed;
+	private BufferedImage [] boom;
+	private boolean atRightEdge;
+	private boolean atLeftEdge;
+	private SpriteSheet planeImage;
+	private BufferedImage originalImg;
+	private static BufferedImage[] sprites1;
+	private JLabel planeImage1;
+	private BufferedImage img;
+	private BufferedImage img1;
+	private Image img2;
+	private Timer timer;
+	private int dx;
+    private int dy;
+    private int x = 40;
+    private int y = 60;
+    private int w;
+    private int h;
+
+	public PlaneSprite() {
+		loadImage();
+	}
+	
+	private void loadImage() {
+		try {
+			originalImg = ImageIO.read(new File("Plane3.png"));
+			planeImage = new SpriteSheet(originalImg, 200, 134, 5, 1);
+			sprites1 = planeImage.getSprites();
+			img1 = sprites1[2];
+			
+			w = img1.getWidth();
+			h = img1.getHeight();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Timer timer = new Timer(8, this);
+		timer.start();
+	}
+	
+	public void move() {
+		x += dx;
+		y += dy;
+	}
+	
+	public int getxPosition() {
+		return x;
+	}
+	public void setxPosition(int xPosition) {
+		this.x = xPosition;
+	}
+	public int getW() {
+		return w;
+	}
+	public int getH() {
+		return h;
+	}
+	public int getyPosition() {
+		return y;
+	}
+	
+	public BufferedImage getPlane()	{
+		
+		return img1;
+	}
+	
+	public BufferedImage[] getSprites() {
+		return sprites1;
+	}
+	public BufferedImage[] getBankLeft() {
+		return bankLeft;
+	}
+	public void setBankLeft(BufferedImage[] bankLeft) {
+		this.bankLeft = bankLeft;
+	}
+	public BufferedImage[] getLeftLevelout() {
+		return leftLevelout;
+	}
+	public void setLeftLevelout(BufferedImage[] leftLevelout) {
+		this.leftLevelout = leftLevelout;
+	}
+	public BufferedImage[] getBankRight() {
+		return bankRight;
+	}
+	public void setBankRight(BufferedImage[] bankRight) {
+		this.bankRight = bankRight;
+	}
+	public BufferedImage[] getRightLevelout() {
+		return rightLevelout;
+	}
+	public void setRightLevelout(BufferedImage[] rightLevelout) {
+		this.rightLevelout = rightLevelout;
+	}
+	
+	public Rectangle getBounds() {
+		return bounds;
+	}
+	public void setBounds(Rectangle bounds) {
+		this.bounds = bounds;
+	}
+	public int getHitsTaken() {
+		return hitsTaken;
+	}
+	public void setHitsTaken(int hitsTaken) {
+		this.hitsTaken = hitsTaken;
+	}
+	public int getRightMovement() {
+		return rightMovement;
+	}
+	public void setRightMovement(int rightMovement) {
+		this.rightMovement = rightMovement;
+	}
+	public int getLeftMovement() {
+		return leftMovement;
+	}
+	public void setLeftMovement(int leftMovement) {
+		this.leftMovement = leftMovement;
+	}
+	public boolean isPlaneHit() {
+		return planeHit;
+	}
+	public void setPlaneHit(boolean planeHit) {
+		this.planeHit = planeHit;
+	}
+	public boolean isPlaneDestroyed() {
+		return planeDestroyed;
+	}
+	public void setPlaneDestroyed(boolean planeDestroyed) {
+		this.planeDestroyed = planeDestroyed;
+	}
+	public BufferedImage[] getBoom() {
+		return boom;
+	}
+	public void setBoom(BufferedImage[] boom) {
+		this.boom = boom;
+	}
+	public boolean isAtRightEdge() {
+		return atRightEdge;
+	}
+	public void setAtRightEdge(boolean atRightEdge) {
+		this.atRightEdge = atRightEdge;
+	}
+	public boolean isAtLeftEdge() {
+		return atLeftEdge;
+	}
+	public void setAtLeftEdge(boolean atLeftEdge) {
+		this.atLeftEdge = atLeftEdge;
+	}
+	public void keyPressed(KeyEvent e) {
+
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+            dx = -2;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+            dx = 2;
+        }
+
+        if (key == KeyEvent.VK_UP) {
+            dy = -2;
+        }
+
+        if (key == KeyEvent.VK_DOWN) {
+            dy = 2;
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+        
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+            dx = 0;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+            dx = 0;
+        }
+
+        if (key == KeyEvent.VK_UP) {
+            dy = 0;
+        }
+
+        if (key == KeyEvent.VK_DOWN) {
+            dy = 0;
+        }
+    }
+	
+}
