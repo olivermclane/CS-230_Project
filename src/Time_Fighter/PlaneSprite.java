9b99dd4fc9765512052dd4008b077fa726bc5e96
dustgard <77804262+dustgard@users.noreply.github.com>
2021-10-07 01:58:32 -0600

    Update PlaneSprite.java

--- src/Time_Fighter/PlaneSprite.java
+++ src/Time_Fighter/PlaneSprite.java
@@ -179,7 +179,7 @@ public class PlaneSprite extends SpriteSheet implements ActionListener, ImageObs
 
 	private void loadImage() {
 		try {
-			originalImg = ImageIO.read(new File("src/Time_Fighter/Plane-assets/Plane3.png"));
+			originalImg = ImageIO.read(new File("src/Plane-assets/Plane3.png"));
 			planeImage = new SpriteSheet(originalImg, 200, 134, 5, 1);
 			sprites1 = planeImage.getSprites();
 			img1 = sprites1[2];
