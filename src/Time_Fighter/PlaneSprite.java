2f91ddda4dab8a0ec0797449d012be553f4e2479
dustgard <77804262+dustgard@users.noreply.github.com>
2021-10-11 14:11:19 -0600

    Update

--- src/Time_Fighter/PlaneSprite.java
+++ src/Time_Fighter/PlaneSprite.java
@@ -65,7 +65,7 @@ public class PlaneSprite extends SpriteSheet
 	private int y1;
 	private boolean didPlaneFire;
 	private Missile missile;
-	private List<Missile> bullets = new ArrayList<>();
+	private List<Missile> missiles = new ArrayList<>();
 
 	public PlaneSprite() {
 		loadImage();
@@ -75,8 +75,10 @@ public class PlaneSprite extends SpriteSheet
 	}
 
 	public boolean fire() {
-		bullets.add(new Missile());
+		missiles.add(new Missile());
 		return true;
+//		bullets.add(new Missile());
+//		return true;
 
 	}
 
@@ -96,10 +98,17 @@ public class PlaneSprite extends SpriteSheet
 		if (planeDown) {
 			moveDown();
 		}
-
 		if (didPlaneFire) {
-			bullets.stream().forEach(bullet -> bullet.doDrawing1(g));
+			for(Missile x:missiles) {
+				x.doDrawing1(g);
+				if(x.getY1()<0) {
+					missiles.remove(x);
+				}
+			}
 		}
+//		if (didPlaneFire) {
+//			bullets.stream().forEach(bullet -> bullet.doDrawing1(g));
+//		}
 //		bullets.removeIf(Missile::getX1()<0);
 	}
 
@@ -228,7 +237,7 @@ public class PlaneSprite extends SpriteSheet
 
 	}
 
-	// same as player 1
+	
 	public void moveRight() {
 		if (x >= 580) {
 			x = 580;
@@ -258,7 +267,7 @@ public class PlaneSprite extends SpriteSheet
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// when keys are released then stop moving
+		
 		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
 			planeRight = false;
 
@@ -369,7 +378,7 @@ public class PlaneSprite extends SpriteSheet
 	public void mouseClicked(MouseEvent e) {
 		this.setxPosition(e.getX());
 		this.setyPosition(e.getY());
-		System.out.println("test");
+		
 
 	}
 
@@ -389,7 +398,7 @@ public class PlaneSprite extends SpriteSheet
 	public void mousePressed(MouseEvent e) {
 		this.setxPosition(e.getX());
 		this.setyPosition(e.getY());
-		System.out.println("test");
+		
 
 	}
 
@@ -405,7 +414,7 @@ public class PlaneSprite extends SpriteSheet
 		x1 = e.getX();
 		y1 = e.getY();
 		this.setyPosition(e.getY());
-		System.out.println("test");
+		
 
 	}
 
@@ -415,89 +424,88 @@ public class PlaneSprite extends SpriteSheet
 		this.setxPosition(e.getX());
 		this.setyPosition(e.getY());
 
-		System.out.println("test");
-
+		
 	}
 
-	public class Missile extends PlaneSprite {
-		private BufferedImage originalImg1;
-		private SpriteSheet planeImage11;
-		private BufferedImage[] sprites11;
-		private BufferedImage img11;
-		private int w1;
-		private int h1;
-
-		public Missile() {
-			loadImage1();
-
-		}
-
-		private void loadImage1() {
-			try {
-
-				originalImg1 = ImageIO.read(new File("src/Plane-assets/Missile2.png"));
-				w1 = originalImg1.getWidth();
-				h1 = originalImg1.getHeight();
-
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-		}
-
-		public void doDrawing1(Graphics r) {
-			if (fire()) {
-
-				r.drawImage(getOriginalImg1(), x1 - 55, y1 - 70, getOriginalImg1().getWidth(),
-						getOriginalImg1().getHeight(), this);
-				System.out.println("BUllets");
-				y1 -= 2;
-			}
-
-//				// Draw the image onto the Graphics reference
-//				g.drawImage(image, getX(), getY(), image.getWidth(), image.getHeight(), this);
+//	public class Missile extends PlaneSprite {
+//		private BufferedImage originalImg1;
+//		private SpriteSheet planeImage11;
+//		private BufferedImage[] sprites11;
+//		private BufferedImage img11;
+//		private int w1;
+//		private int h1;
+//		private Missile shoot;
+//		public Missile() {
+//			loadImage1();
+//			
+//		}
 //
-//				// Move the x position left for next time
-//				this.y += 2;
+//		private void loadImage1() {
+//			try {
 //
-//				// Check to see if the image has gone off stage left
-//				if (this.y >= image.getHeight() - 700) {
+//				originalImg1 = ImageIO.read(new File("src/Plane-assets/Missile2.png"));
+//				w1 = originalImg1.getWidth();
+//				h1 = originalImg1.getHeight();
 //
-//					// If it has, line it back up so that its left edge is
-//					// lined up to the right side of the other background image
-//					this.y = (this.y - image.getHeight() * 2);
-			else {
-
-			}
-		}
-
-		public int getX1() {
-			return x1;
-		}
-
-		public void setX1(int x) {
-			x1 = x;
-		}
-
-		public int getY1() {
-			return y1;
-		}
-
-		public void setY1(int y) {
-			y1 = y;
-		}
-
-		public BufferedImage getOriginalImg1() {
-			return originalImg1;
-		}
-
-	}
-
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-
-	}
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//
+//		}
+//
+//		public void doDrawing1(Graphics r) {
+//			if (didPlaneFire) {
+//
+//				r.drawImage(getOriginalImg1(), x1 - 55, y1 - 70, getOriginalImg1().getWidth(),
+//						getOriginalImg1().getHeight(), this);
+//				
+//				y1 -= 2;
+//			}
+//
+////				// Draw the image onto the Graphics reference
+////				g.drawImage(image, getX(), getY(), image.getWidth(), image.getHeight(), this);
+////
+////				// Move the x position left for next time
+////				this.y += 2;
+////
+////				// Check to see if the image has gone off stage left
+////				if (this.y >= image.getHeight() - 700) {
+////
+////					// If it has, line it back up so that its left edge is
+////					// lined up to the right side of the other background image
+////					this.y = (this.y - image.getHeight() * 2);
+//			else {
+//
+//			}
+//		}
+//
+//		public int getX1() {
+//			return x1;
+//		}
+//
+//		public void setX1(int x) {
+//			x1 = x;
+//		}
+//
+//		public int getY1() {
+//			return y1;
+//		}
+//
+//		public void setY1(int y) {
+//			y1 = y;
+//		}
+//
+//		public BufferedImage getOriginalImg1() {
+//			return originalImg1;
+//		}
+//
+//	}
+//
+//	@Override
+//	public void run() {
+//		// TODO Auto-generated method stub
+//
+//	}
 
 }
