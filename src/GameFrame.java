0a7d7a295a305c92fa54e7489d8d8c700ba304a9
Oliver McLane <o@mclane.cc>
2021-10-21 11:32:12 -0600

    Added back GameFrame.java need for something needs to be fix in our intergration testing

---
+++ src/GameFrame.java
@@ -0,0 +1,26 @@
+/**
+ *
+ */
+
+import javax.swing.JFrame;
+
+/**
+ *
+ *
+ */
+public class GameFrame extends JFrame {
+
+	public GameFrame() {
+		initFrame();
+
+	}
+
+	private void initFrame() {
+		setTitle("Test");
+		setSize(700, 800);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+}
